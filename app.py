c929030dc6c7b63f6d0436a135849922ba24a5fc
Youssef Girgeis <youssef_girgeis@hotmail.com>
2023-10-05 16:08:45 -0400

    create a venue

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, request
 from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 from forms import *
 
 app = Flask(__name__)
@@ -7,31 +8,32 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://admin:admin123@localhost:5
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 app.app_context().push()
+migrate = Migrate(app, db)
 
 
 class Artist(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String())
-    city = db.Column(db.String(120))
-    state = db.Column(db.String(120))
+    name = db.Column(db.String(), nullable=False)
+    city = db.Column(db.String(120), nullable=False)
+    state = db.Column(db.String(120), nullable=False)
     phone = db.Column(db.String(120))
     genres = db.Column(db.String(120))
     image_link = db.Column(db.String(500))
     facebook_link = db.Column(db.String(120))
+    website_link = db.Column(db.String(120))
 
 
 class Venue(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String)
-    city = db.Column(db.String(120))
-    state = db.Column(db.String(120))
+    name = db.Column(db.String(), nullable=False)
+    city = db.Column(db.String(120), nullable=False)
+    state = db.Column(db.String(120), nullable=False)
     address = db.Column(db.String(120))
     phone = db.Column(db.String(120))
+    genres = db.Column(db.String(120))
     image_link = db.Column(db.String(500))
     facebook_link = db.Column(db.String(120))
-
-
-db.create_all()
+    website_link = db.Column(db.String(120))
 
 
 @app.route('/')
@@ -62,7 +64,7 @@ def create_artist_submission():
     # print(request.form['name'])
     artist = Artist(name=request.form['name'], city=request.form['city'],
                     state=request.form['state'], phone=request.form['phone'], genres=request.form['genres'],
-                    image_link=request.form['image_link'], facebook_link=request.form['facebook_link'])
+                    image_link=request.form['image_link'], facebook_link=request.form['facebook_link'], website_link=request.form['website_link'])
     db.session.add(artist)
     db.session.commit()
     return render_template('pages/home.html')
@@ -81,10 +83,21 @@ def edit_artist(artist_id):
     return render_template('forms/edit_artist.html', form=form, artist=artist)
 
 # VENUE ROUTES AND CONTROLLERS
-@app.route('/venues/create', methods=['GET'])
+
+
+@app.route('/venues/create', methods=['GET', 'POST'])
 def create_venue_form():
-    venue_form = VenueForm()
-    return render_template('forms/new_venue.html', form=venue_form)
+    print(request.form)
+    if request.method == 'GET':
+        venue_form = VenueForm()
+        return render_template('forms/new_venue.html', form=venue_form)
+    else:
+        venue = Venue(name=request.form['name'], city=request.form['city'], address=request.form['address'],
+                      state=request.form['state'], phone=request.form['phone'], genres=request.form['genres'],
+                      image_link=request.form['image_link'], facebook_link=request.form['facebook_link'], website_link=request.form['website_link'])
+        db.session.add(venue)
+        db.session.commit()
+        return render_template('pages/home.html')
 
 
 if __name__ == '__main__':

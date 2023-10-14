164d8837693a67d2389e9ba2f34a7b55d3fec874
Youssef Girgeis <youssef_girgeis@hotmail.com>
2023-10-14 18:32:11 -0400

    show, edit and show all venues

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect, url_for
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from forms import *
@@ -61,7 +61,6 @@ def create_artist_form():
 
 @app.route('/artists/create', methods=['POST'])
 def create_artist_submission():
-    # print(request.form['name'])
     artist = Artist(name=request.form['name'], city=request.form['city'],
                     state=request.form['state'], phone=request.form['phone'], genres=request.form['genres'],
                     image_link=request.form['image_link'], facebook_link=request.form['facebook_link'], website_link=request.form['website_link'])
@@ -87,7 +86,6 @@ def edit_artist(artist_id):
 
 @app.route('/venues/create', methods=['GET', 'POST'])
 def create_venue_form():
-    print(request.form)
     if request.method == 'GET':
         venue_form = VenueForm()
         return render_template('forms/new_venue.html', form=venue_form)
@@ -98,6 +96,52 @@ def create_venue_form():
         db.session.add(venue)
         db.session.commit()
         return render_template('pages/home.html')
+
+
+@app.route('/venues', methods=['GET'])
+def show_all_venues():
+    areas = {}
+    res = Venue.query.all()
+    for venue in res:
+        if venue.city in areas:
+            areas[venue.city].append(venue)
+        else:
+            areas[venue.city] = [venue]
+
+    return render_template('pages/venues.html', areas=areas)
+
+
+@app.route('/venues/<int:venue_id>')
+def show_venue(venue_id):
+    venue = Venue.query.get(venue_id)
+    return render_template('pages/show_venue.html', venue=venue)
+
+
+@app.route('/venues/<int:venue_id>/edit', methods=['GET', 'POST'])
+def edit_venue(venue_id):
+    venue = Venue.query.get(venue_id)
+    if request.method == 'GET':
+        venue_form = VenueForm()
+        venue_form.name.data = venue.name
+        venue_form.city.data = venue.city
+        venue_form.state.data = venue.state
+        venue_form.address.data = venue.address
+        venue_form.phone.data = venue.phone
+        venue_form.genres.data = venue.genres
+        venue_form.facebook_link.data = venue.facebook_link
+        venue_form.website_link.data = venue.website_link
+        return render_template('forms/edit_venue.html', form=venue_form, venue=venue)
+    else:
+        venue.name = request.form['name']
+        venue.city = request.form['city']
+        venue.state = request.form['state']
+        venue.address = request.form['address']
+        venue.phone = request.form['phone']
+        venue.geners = request.form['genres']
+        venue.facebook_link = request.form['facebook_link']
+        venue.website_link = request.form['website_link']
+        db.session.commit()
+        return redirect(url_for('show_venue', venue_id=venue_id))
 
 
 if __name__ == '__main__':
